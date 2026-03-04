4f3895f7c0c4b9f58f1b421cd1020d8ae3c28c7d
HudsonWayne <waynewaynebenhura@gmail.com>
2026-03-04 08:19:38 +0200

    initial commit

--- variables.java
+++ variables.java
@@ -18,7 +18,11 @@ public class variables {
 
 
         
-        System.out.println("The year is " + year);
-
+        if (isStudent) {
+            System.out.println("You are a student");
+        }
+        else{
+            System.out.println("You are NOT a student");
+        }
     }
 }
