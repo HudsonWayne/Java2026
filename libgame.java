92a18bb606d6828e6169e3d51d4ddc99f36f53d4
HudsonWayne <waynewaynebenhura@gmail.com>
2026-03-04 13:50:47 +0200

    initial commit

--- libgame.java
+++ libgame.java
@@ -1,40 +1,41 @@
 import java.util.Scanner;
 
-
 public class libgame {
+
     public static void main(String[] args) {
 
-        //Mid Lib game
+        // Mad Lib Game
 
-        Scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
         String adjective1;
         String noun1;
         String adjective2;
         String verb1;
+        String adjective3;
 
+        System.out.print("Enter an adjective (description): ");
+        adjective1 = scanner.nextLine();
 
+        System.out.print("Enter a noun (animal or person): ");
+        noun1 = scanner.nextLine();
 
-        System.out.println("Enter an adjective (description): ");
-        adjective1 = scanner.nextLine()
-        System.out.println("Enter a noun (animal or person): ");
-        System.out.println("Enter an adjective (description): ");
-        System.out.println("Enter a verb with -ing (action)");
+        System.out.print("Enter another adjective (description): ");
+        adjective2 = scanner.nextLine();
 
+        System.out.print("Enter a verb ending with -ing (action): ");
+        verb1 = scanner.nextLine();
 
-        System.out.println("Today i went to a " + adjective1 + " zoo ");
-        System.out.println("In an exhibit, i saw a " + noun1 + ".");
+        System.out.print("Enter another adjective (feeling): ");
+        adjective3 = scanner.nextLine();
+
+        System.out.println("\n--- Your Story ---\n");
+
+        System.out.println("Today I went to a " + adjective1 + " zoo.");
+        System.out.println("In an exhibit, I saw a " + noun1 + ".");
         System.out.println(noun1 + " was " + adjective2 + " and " + verb1 + "!");
         System.out.println("I was " + adjective3 + "!");
 
-
-
-
-
-
-        System.out.println();
-
-
-        
+        scanner.close();
     }
 }
