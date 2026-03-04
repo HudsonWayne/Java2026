4927b21833467dd50e4b8afa9043dd63c3339e24
HudsonWayne <waynewaynebenhura@gmail.com>
2026-03-04 11:53:45 +0200

    initial commit

--- libgame.java
+++ libgame.java
@@ -15,8 +15,13 @@ public class libgame {
 
         System.out.println("Today i went to a " + adjective1 + " zoo ");
         System.out.println("In an exhibit, i saw a " + noun1 + ".");
-
         System.out.println(noun1 + " was " + adjective2 + " and " + verb1 + "!");
+        System.out.println("I was " + adjective3 + "!");
+
+
+
+
+
 
         System.out.println();
 
