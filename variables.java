f496aade0b40ead1384fbd1c27a66103a6d10136
HudsonWayne <waynewaynebenhura@gmail.com>
2026-03-04 07:57:33 +0200

    initial commit

--- variables.java
+++ variables.java
@@ -1,7 +1,7 @@
 public class variables {
-    
 
-    int age = 22;
-    System.out.printIn(age);
-    
+    public static void main(String[] args) {
+        int age = 22;
+        System.out.println(age);
+    }
 }
