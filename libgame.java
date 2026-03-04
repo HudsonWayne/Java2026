596ae15751a53ee6259156e03e60267a1cb662f7
HudsonWayne <waynewaynebenhura@gmail.com>
2026-03-04 09:53:56 +0200

    initial commit

--- libgame.java
+++ libgame.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+
+
 public class libgame {
     public static void main(String[] args) {
+
+        //Mid Lib game
+
+        Scanner = new Scanner(System.in);
+
+        String adjective1;
+
         
     }
 }
