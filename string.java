e677b828a544bc93fc2c3bbd3ff80a9bf661297e
HudsonWayne <waynewaynebenhura@gmail.com>
2026-03-06 11:37:48 +0200

    initial commit

--- string.java
+++ string.java
@@ -1,6 +1,9 @@
 public class string {
     public static void main(String[] args) {
         String name = "Bro Code";
+
+        int length = name.length();
+        System.out.println(length);
         
     }
 }
